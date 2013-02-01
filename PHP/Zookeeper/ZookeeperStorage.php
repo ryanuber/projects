e198d7119fd78eae1d1c7dbf0823c67ab7a0e55c
Ryan Uber <ru@ryanuber.com>
2013-02-01 12:33:27 -0800

    Adding get_recursive to zookeeper storage class

--- PHP/Zookeeper/ZookeeperStorage.php
+++ PHP/Zookeeper/ZookeeperStorage.php
@@ -10,6 +10,8 @@
  *     paths to znodes.
  *   - Managing a single connection to the Zookeeper server while the class
  *     instance exists.
+ *   - Can traverse all znodes below a certain hierarchy and gather data
+ *     from each child znode.
  */
 class ZookeeperStorage
 {
@@ -28,6 +30,20 @@ class ZookeeperStorage
     public static $instance;
 
     /**
+     * A symbolic pointer to an element inside of the result array
+     *
+     * @pointer $pointer
+     */
+    private static $pointer;
+
+    /**
+     * A placeholder for query results. Useful during recursion.
+     *
+     * @var mixed $result
+     */
+    private static $result;
+
+    /**
      * Constructor function which actually initiates the connection to
      * Zookeeper, rather than requiring the programmer to call some connect
      * method after instantiating.
@@ -36,10 +52,10 @@ class ZookeeperStorage
      */
     public function __construct($host='127.0.0.1', $port=2181)
     {
-        if(! self::$instance instanceof ZookeeperStorage) {
+        if(! self::$instance instanceof \ZookeeperStorage) {
             try {
-                self::$Zookeeper = new Zookeeper($host.':'.$port);
-            } catch(Exception $Error) {
+                self::$Zookeeper = new \Zookeeper($host.':'.$port);
+            } catch(\Exception $Error) {
                 self::setLastError(
                     'Failed connecting to Zookeeper host '.
                     $host.' on port '.$port
@@ -48,6 +64,8 @@ class ZookeeperStorage
 
             self::$instance = $this;
         }
+
+        self::$pointer = &self::$result;
 
         return self::$instance;
     }
@@ -136,6 +154,39 @@ class ZookeeperStorage
     }
 
     /**
+     * As much as possible, recurse through znodes in Zookeeper starting at $path and
+     * collect all data into an associative array. Zookeeper supports atypical data
+     * structure where a znode can be a parent and contain data. This method will ignore
+     * data set on a znode that is a parent since arrays in most programming languages
+     * do not support such a data structure.
+     *
+     * @param string $path  The path to begin recursion from
+     * @param bool $return  Simple internal indicator we use to know when to return
+     *
+     * @return mixed  Array on success, null on failure.
+     */
+    public function get_recursive($path='', $return=true)
+    {
+        foreach(self::get_list($path) as $leaf) {
+            if(count(self::get_list(self::path_join($path, $leaf))) > 0) {
+                self::$pointer[$leaf] = array();
+                $p = &self::$pointer;
+                self::$pointer = &self::$pointer[$leaf];
+                self::get_recursive(self::path_join($path, $leaf), false);
+                self::$pointer = &$p;
+            } else {
+                self::$pointer[$leaf] = self::get(self::path_join($path, $leaf));
+            }
+        }
+        if ($return) {
+            $result = self::$result;
+            self::$result = null;
+            self::$pointer = &self::$result;
+            return $result;
+        }
+    }
+
+    /**
      * Delete a node, first trying to list children, and recursing to delete them, and
      * their children, unifying calls to delete a single node, or a node with children.
      *
@@ -155,7 +206,7 @@ class ZookeeperStorage
 
             try {
                 self::$Zookeeper->delete($path);
-            } catch(Exception $Error) {
+            } catch(\Exception $Error) {
                 self::setLastError('Failed while deleting node '.$path);
                 return FALSE;
             }
@@ -179,7 +230,7 @@ class ZookeeperStorage
 
         try {
             $result = self::$Zookeeper->exists($path)?self::$Zookeeper->get($path):null;
-        } catch(Exception $Error) {
+        } catch(\Exception $Error) {
             self::setLastError('Failed trying to fetch node '.$path);
             return FALSE;
         }
@@ -205,7 +256,7 @@ class ZookeeperStorage
             try {
                 self::$Zookeeper->set($path, $value);
                 return TRUE;
-            } catch(Exception $Error) {
+            } catch(\Exception $Error) {
                 self::setLastError('Failed trying to update existing node '.$path);
                 return FALSE;
             }
@@ -223,9 +274,9 @@ class ZookeeperStorage
                 self::$Zookeeper->create(
                     $path,
                     $value,
-                    array(array('perms' => Zookeeper::PERM_ALL, 'scheme' => 'world', 'id' => 'anyone'))
+                    array(array('perms' => \Zookeeper::PERM_ALL, 'scheme' => 'world', 'id' => 'anyone'))
                 );
-            } catch(Exception $Error) {
+            } catch(\Exception $Error) {
                 self::setLastError('Failed tyring to create new node '.$path);
                 return FALSE;
             }
